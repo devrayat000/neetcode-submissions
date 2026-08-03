6b5c9919b5d6e32f4170c1a162adac2178a567fa
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2026-08-03 11:49:22 +0600

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0], profit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            profit = max(prices[i] - buy, profit);
+            buy = min(prices[i], buy);
+        }
+        return profit < 0 ? 0 : profit;
+    }
+};
